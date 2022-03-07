f5fabf9ea0a7c15865af44e2a65eda18b9ed38ac
p-alex <alexey9021@gmail.com>
2022-03-08 01:12:43 +0200

    Remove sameSite from cookie

--- src/security/jwt.ts
+++ src/security/jwt.ts
@@ -15,7 +15,6 @@ export const setRefreshTokenCookie = (res: Response, refreshToken: string) => {
   res.cookie('refreshToken', refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
     maxAge: parseInt(process.env.REFRESH_TOKEN_COOKIE_EXPIRE!),
   });
 };
