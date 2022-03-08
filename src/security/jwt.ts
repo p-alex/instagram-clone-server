20974e3eb915fee29483761831c19eed5d651e3e
p-alex <alexey9021@gmail.com>
2022-03-08 20:29:01 +0200

    Add domain to cookie

--- src/security/jwt.ts
+++ src/security/jwt.ts
@@ -16,5 +16,6 @@ export const setRefreshTokenCookie = (res: Response, refreshToken: string) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'none',
+    domain: 'instagram-clone-9021',
   });
 };
