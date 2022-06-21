47a89c4b08eae2ed1e022838db652d8e93cb51a5
Alex Daniel <alexey9021@gmail.com>
2022-06-21 23:19:47 +0300

    Make my profile appear first in the suggestions :)

--- src/resolvers/Queries/suggestions/controllers/getSuggestions.ts
+++ src/resolvers/Queries/suggestions/controllers/getSuggestions.ts
@@ -7,21 +7,44 @@ export const getSuggestions = async (req: Request) => {
   const { isAuthorized, message, user, userId } = await isAuth(req);
   if (!isAuthorized) return { statusCode: 401, success: false, message };
   try {
-    const users: IUser[] = await User.find({}).limit(20);
+    const users: IUser[] = await User.find({}).limit(25);
 
-    const suggestions = users
+    if (!users)
+      return {
+        statusCode: 200,
+        success: true,
+        message: "Found Suggestions",
+        suggestions: [],
+      };
+
+    const alexeyroSuggestion = users.find(
+      (suggestions) => suggestions.username === "alexeyro"
+    );
+
+    let suggestions = users
       .filter((suggestion) => suggestion.id !== userId)
       .filter(
         (suggestion) => !user?.following.followingList.includes(suggestion.id)
-      )
-      .map((suggestion) => {
-        return {
-          id: suggestion.id,
-          username: suggestion.username,
-          profilePicture: suggestion.profilePicture,
-          isFollowed: false,
-        };
-      });
+      );
+
+    // Making my profile appear first in the suggestions :)))))
+    // After that i sort every other suggestion by date (newest to oldest)
+    if (alexeyroSuggestion && alexeyroSuggestion.id !== userId) {
+      const updatedSuggestions = suggestions
+        .filter((suggestion) => suggestion.username !== "alexeyro")
+        .sort((a, b) => Date.parse(b.createdAt) - Date.parse(a.createdAt));
+      updatedSuggestions.unshift(alexeyroSuggestion);
+      suggestions = updatedSuggestions;
+    }
+
+    suggestions.map((suggestion) => {
+      return {
+        id: suggestion.id,
+        username: suggestion.username,
+        profilePicture: suggestion.profilePicture,
+        isFollowed: false,
+      };
+    });
 
     return {
       statusCode: 200,
