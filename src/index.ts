fe15587e2d0f3399429ff039ec176d2139f76b7b
Alex Daniel <alexey9021@gmail.com>
2022-07-11 22:56:12 +0300

    Add helmet and remove cors package

--- src/index.ts
+++ src/index.ts
@@ -6,28 +6,15 @@ import express from "express";
 import http from "http";
 import typeDefs from "./schema";
 import resolvers from "./resolvers/index";
-import cors from "cors";
 import { DocumentNode } from "graphql";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
 
 const startApolloServer = async (typeDefs: DocumentNode, resolvers: any) => {
   const app = express();
 
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-    next();
-  });
-
-  app.use(
-    cors({
-      origin:
-        process.env.NODE_ENV === "production"
-          ? ["https://bubble.pistolalex.com"]
-          : ["http://localhost:3000", "https://studio.apollographql.com"],
-      credentials: true,
-    })
-  );
+  app.use(helmet());
 
   app.use(express.json({ limit: "650kb" }));
   app.use(express.urlencoded({ limit: "650kb", extended: true }));
