ba7ec39069e38d2ec0af096c36774b9d1ddf515c
p-alex <alexey9021@gmail.com>
2023-05-19 23:26:09 +0300

    Railway redeploy

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,34 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import cors from 'cors';
-import { ApolloServer } from 'apollo-server-express';
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core/dist/plugin/drainHttpServer';
-import express from 'express';
-import http from 'http';
-import typeDefs from './schema';
-import resolvers from './resolvers/index';
-import { DocumentNode } from 'graphql';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
-import helmet from 'helmet';
-
+import cors from "cors";
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core/dist/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import typeDefs from "./schema";
+import resolvers from "./resolvers/index";
+import { DocumentNode } from "graphql";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+//ping
 const startApolloServer = async (typeDefs: DocumentNode, resolvers: any) => {
   const app = express();
 
   app.use(
     cors({
       origin:
-        process.env.NODE_ENV === 'production'
-          ? ['https://bubble.pistolalex.com']
-          : ['http://localhost:3000', 'https://studio.apollographql.com'],
+        process.env.NODE_ENV === "production"
+          ? ["https://bubble.pistolalex.com"]
+          : ["http://localhost:3000", "https://studio.apollographql.com"],
       credentials: true,
     })
   );
 
   app.use(helmet());
 
-  app.use(express.json({ limit: '650kb' }));
-  app.use(express.urlencoded({ limit: '650kb', extended: true }));
+  app.use(express.json({ limit: "650kb" }));
+  app.use(express.urlencoded({ limit: "650kb", extended: true }));
 
   app.use(cookieParser());
 
@@ -47,9 +47,9 @@ const startApolloServer = async (typeDefs: DocumentNode, resolvers: any) => {
     app,
     cors: {
       origin:
-        process.env.NODE_ENV === 'production'
-          ? ['https://bubble.pistolalex.com']
-          : ['http://localhost:3000', 'https://studio.apollographql.com'],
+        process.env.NODE_ENV === "production"
+          ? ["https://bubble.pistolalex.com"]
+          : ["http://localhost:3000", "https://studio.apollographql.com"],
       credentials: true,
     },
   });
@@ -57,16 +57,18 @@ const startApolloServer = async (typeDefs: DocumentNode, resolvers: any) => {
   const port = process.env.PORT || 5001;
 
   await mongoose.connect(
-    process.env.NODE_ENV === 'production'
+    process.env.NODE_ENV === "production"
       ? process.env.MONGO_URI!
-      : 'mongodb://localhost:27017/instagramDB',
+      : "mongodb://localhost:27017/instagramDB",
     async () => {
       console.log(
-        process.env.NODE_ENV === 'production'
-          ? '🚀 Database connected'
-          : '🚀 Development Database connected'
+        process.env.NODE_ENV === "production"
+          ? "🚀 Database connected"
+          : "🚀 Development Database connected"
       );
-      await new Promise<void>((resolve) => httpServer.listen({ port }, resolve));
+      await new Promise<void>((resolve) =>
+        httpServer.listen({ port }, resolve)
+      );
       console.log(`🚀 Server running at http://localhost:${port}/graphql`);
     }
   );
